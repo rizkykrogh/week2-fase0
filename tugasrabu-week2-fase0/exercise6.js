ca3dd2ce74d3f10fe8e2cbfceac82d0f1ffea2ee
rizkykrogh <rizky.krogh@icloud.com>
2020-02-02 15:03:38 +0700

    Update exercise6.js

--- tugasrabu-week2-fase0/exercise6.js
+++ tugasrabu-week2-fase0/exercise6.js
@@ -1,1 +1,38 @@
-var i = 0
+// 1
+var i = 0;
+
+console.log('LOOPING PERTAMA');
+while(i < 20) {
+    i += 2;
+    console.log(i + ' - I Love Coding');
+}
+console.log('LOOPING KEDUA');
+while(i > 0) {
+    i -= 2;
+    console.log(i + ' - I will become FullStack Developer')
+}
+
+// 2
+console.log('LOOPING PERTAMA');
+for(var i = 1; i <= 20; i++) {
+    console.log(i + ' - I Love Coding');
+}
+console.log('LOOPING KEDUA');
+for(var i = 20; i >= 1; i--) {
+    console.log(i + ' - I will become FullStack Developer')
+}
+
+// 3
+for(var counter = 1; counter <= 100; counter++) {
+    if(counter % 2 === 0) {console.log('GENAP');}
+        else {console.log('GANJIL');}
+}
+for(var counter = 1; counter <= 100; counter += 2) {
+    if(counter % 3 === 0) {console.log(' kelipatan 3')}
+}
+for(var counter = 1; counter <= 100; counter += 5) {
+    if(counter % 6 === 0) {console.log(' kelipatan 6')}
+}
+for(var counter = 1; counter <= 100; counter += 9) {
+    if(counter % 10 === 0) {console.log(' kelipatan 10')}
+}
